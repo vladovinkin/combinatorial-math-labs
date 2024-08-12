74e80ea397a0f1e3fa8a46fa60df47bbedf257c4
vladovinkin@book <vladovinkin@gmail.com>
2024-08-12 23:28:41 +0300

    - добавил тесты

--- lab4/TestTreeCount/TestTreeCount.cpp
+++ lab4/TestTreeCount/TestTreeCount.cpp
@@ -3,3 +3,72 @@
 #include "../../../oop-homeworks/catch2/catch.hpp"
 #include "../TreeCount/TreeCount.h"
 
+
+/*
+      1   4
+     / \
+    2---3
+*/
+
+TEST_CASE("Тест 0")
+{
+    setlocale(LC_ALL, "rus");
+
+    Matrix m{
+       { 0, 1, 1, 0 },
+       { 1, 0, 1, 0 },
+       { 1, 1, 0, 0 },
+       { 0, 0, 0, 0 },
+    };
+
+    auto tc = TreeCount(m);
+
+    Matrix km = tc.GetKirchhoffMatrix();
+    CHECK(tc.GetCountTree() == 0);
+}
+
+/*
+    1
+  / | \
+ 2--3--4
+
+ */
+TEST_CASE("Тест 1")
+{
+	setlocale(LC_ALL, "rus");
+
+    Matrix m{
+       { 0, 1, 1, 1 },
+       { 1, 0, 1, 0 },
+       { 1, 1, 0, 1 },
+       { 1, 0, 1, 0 },
+    };
+
+    auto tc = TreeCount(m);
+
+    Matrix km = tc.GetKirchhoffMatrix();
+    CHECK(tc.GetCountTree() == 8);
+}
+
+TEST_CASE("Пример из лекции")
+{
+    setlocale(LC_ALL, "rus");
+
+    Matrix m{
+       { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
+       { 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
+       { 0, 1, 0, 0, 1, 1, 0, 0, 0, 0 },
+       { 1, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+       { 0, 0, 1, 0, 0, 1, 0, 1, 0, 1 },
+       { 0, 1, 1, 1, 1, 0, 0, 1, 1, 0 },
+       { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 },
+       { 0, 0, 0, 0, 1, 1, 0, 0, 1, 1 },
+       { 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 },
+       { 0, 0, 0, 0, 1, 0, 0, 1, 1, 0 },
+    };
+
+    auto tc = TreeCount(m);
+
+    Matrix km = tc.GetKirchhoffMatrix();
+    CHECK(tc.GetCountTree() == 3422);
+}
